691db28d080baaa7163b463d180b6b2e2e28969a
tanmayshankar <tanmay.shankar@gmail.com>
2017-02-23 17:51:40 -0500

    Update

--- scripts/Visualize_Trajectories.py
+++ scripts/Visualize_Trajectories.py
@@ -17,10 +17,9 @@ def publish_trajectory(traj,traj_shoulder):
 
 	traj_pub = rospy.Publisher("trajectory_markers",Marker,queue_size=10)
 	traj_pub_shoulder = rospy.Publisher("shoulder_trajectory_markers",Marker,queue_size=10)
-	rate = rospy.Rate(10)
+	rate = rospy.Rate(20)
 	traj_marker = Marker()
 	shoulder_marker = Marker()
-
 
 	# traj_marker.header.frame_id = "rgb_optical_frame"
 	traj_marker.header.frame_id = "traj_frame"
@@ -31,7 +30,8 @@ def publish_trajectory(traj,traj_shoulder):
 	traj_marker.scale.x = 0.0001
 	traj_marker.scale.y = 0.0001
 	traj_marker.scale.z = 0.0001
-	traj_marker.color.b = 1.0
+	traj_marker.color.g = 1.0
+	traj_marker.color.b = 0.4
 	traj_marker.color.a = 1.0
 
 	shoulder_marker.header.frame_id = "traj_frame"
@@ -42,21 +42,21 @@ def publish_trajectory(traj,traj_shoulder):
 	shoulder_marker.scale.x = 0.0001
 	shoulder_marker.scale.y = 0.0001
 	shoulder_marker.scale.z = 0.0001
-	shoulder_marker.color.g = 1.0
+	shoulder_marker.color.r = 1.0
+	shoulder_marker.color.b = 0.4
 	shoulder_marker.color.a = 1.0
-
 
 	p = Point()
 	for i in range(len(traj)):
-		p.x = -traj[i,0]
-		p.y = traj[i,1]
+		p.x = traj[i,0]
+		p.y = -traj[i,1]
 		p.z = traj[i,2]
 
 		traj_marker.points.append(copy.deepcopy(p))
 
 	for i in range(len(traj_shoulder)):
-		p.x = -traj_shoulder[i,0]
-		p.y = traj_shoulder[i,1]
+		p.x = traj_shoulder[i,0]
+		p.y = -traj_shoulder[i,1]
 		p.z = traj_shoulder[i,2]
 
 		shoulder_marker.points.append(copy.deepcopy(p))
@@ -70,9 +70,14 @@ def publish_trajectory(traj,traj_shoulder):
 def main(argv):
 	rospy.init_node("Trajectory_Visualize")
 
+	FILE_DIR = "/home/tanmay/Research/Code/ActionPrimitives/Data/Cornell_Data/Subject1_rgbd_images/"
 	TRAJ_DIR = "/home/tanmay/Research/Code/ActionPrimitives/Data/Cornell_Data/Primitive_Library/Subject1"
 
-	traj_ind = 8
+	sorting_indices = npy.load(os.path.join(FILE_DIR,"Sorting_Indices.npy"))
+	
+	traj_ind = sorting_indices[int(sys.argv[1])]
+	# traj_ind = int(sys.argv[1])
+	# traj_ind = 8
 	path = os.path.join(TRAJ_DIR,"Traj_{0}".format(traj_ind),"Original_Left_Hand_{0}.npy".format(traj_ind))
 	path_sh = os.path.join(TRAJ_DIR,"Traj_{0}".format(traj_ind),"Original_Left_Shoulder_{0}.npy".format(traj_ind))
 	# path = os.path.join(TRAJ_DIR,"Traj_{0}".format(traj_ind),"Original_Right_Hand_{0}.npy".format(traj_ind))
